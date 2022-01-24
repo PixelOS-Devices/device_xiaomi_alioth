bab637c3b1d03bb1637048ed99cf12f25ce50a16
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-01-24 15:22:12 +0530

    alioth: overlay: Set default Settings.Global.DEVICE_NAME according to MIUI
    * MIUI uses ro.product.vendor.marketname for it
    
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- device.mk
+++ device.mk
@@ -400,7 +400,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     AliothFrameworks \
-    AliothSystemUI
+    AliothSystemUI \
+    SettingsProviderM2012K11AC \
+    SettingsProviderM2012K11AG \
+    SettingsProviderM2012K11AI 
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
