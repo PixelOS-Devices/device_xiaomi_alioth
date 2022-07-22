565974afd70c01a3a9bda93483afc6cfd3eb5e83
johnmart19 <johnivan19999@gmail.com>
2022-07-22 12:49:46 +0300

    Alioth: Bringup for VoidUI Project
    - Use aosp_alioth as per our vendor/aosp bringup
    - Use Proper kernel config, required to be used with Immensity kernel
    - Remove lineage.dependencies fine, add aosp.dependencies as per our project
    - Import my [johnmart19] VoidUI Maintainer Flags

---
+++ aosp_alioth.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2022 VoidUI Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common VoidUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device Specific Flags
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# VoidUI Environment Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+TARGET_USES_MINI_GAPPS := false
+
+# VoidUI Maintainer Flags
+VOID_MAINTAINER := johnmart19
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
+PRODUCT_NAME := aosp_alioth
+PRODUCT_DEVICE := alioth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/SKQ1.211006.001/V13.0.3.0.SKHMIXM:user/release-keys
