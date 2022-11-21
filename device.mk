c4fb02e26e7def606a65e76c7c7d483f0b7aa54e
NotZeetaa <rodrigo2005contente@gmail.com>
2022-11-21 19:32:14 +0000

    alioth: Import rounded corners from aospa
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>
    Co-Authored-By: Dyneteve <dyneteve@hentaios.com>
    Co-Authored-By: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+PRODUCT_PACKAGES += \
+    AliothSystemUI
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
