5dbd80382f366e2b53303b0222eeda7db97d2cd3
Gustavo Mendes <gusttavo.me@outlook.com>
2023-02-14 18:41:07 -0300

    alioth: Use full resources from devicesettings
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
@@ -194,9 +194,9 @@ public class RefreshSettingsFragment extends PreferenceFragment
 
     private int getStateDrawable(int state) {
         switch (state) {
-            case RefreshUtils.STATE_STANDARD:
+            case RefreshUtils.STATE_MEDIUM:
                 return R.drawable.ic_refresh_60;
-            case RefreshUtils.STATE_EXTREME:
+            case RefreshUtils.STATE_MAXIMUM:
                 return R.drawable.ic_refresh_120;
             case RefreshUtils.STATE_DEFAULT:
             default:
@@ -228,8 +228,8 @@ public class RefreshSettingsFragment extends PreferenceFragment
         private final LayoutInflater inflater;
         private final int[] items = {
                 R.string.refresh_default,
-                R.string.refresh_standard,
-                R.string.refresh_extreme
+                R.string.refresh_medium,
+                R.string.refresh_maximum
         };
 
         private ModeAdapter(Context context) {
--- parts/src/org/lineageos/settings/refreshrate/RefreshUtils.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshUtils.java
@@ -37,15 +37,15 @@ public final class RefreshUtils {
     protected static boolean isAppInList = false;
 
     protected static final int STATE_DEFAULT = 0;
-    protected static final int STATE_STANDARD = 1;
-    protected static final int STATE_EXTREME = 2;
+    protected static final int STATE_MEDIUM = 1;
+    protected static final int STATE_MAXIMUM = 2;
 
     private static final float REFRESH_STATE_DEFAULT = 60f;
-    private static final float REFRESH_STATE_STANDARD = 60f;
-    private static final float REFRESH_STATE_EXTREME = 120f;
+    private static final float REFRESH_STATE_MEDIUM = 60f;
+    private static final float REFRESH_STATE_MAXIMUM = 120f;
 
-    private static final String REFRESH_STANDARD = "refresh.standard=";
-    private static final String REFRESH_EXTREME = "refresh.extreme=";
+    private static final String REFRESH_MEDIUM = "refresh.medium=";
+    private static final String REFRESH_MAXIMUM = "refresh.maximum=";
 
     private SharedPreferences mSharedPrefs;
 
@@ -73,7 +73,7 @@ public final class RefreshUtils {
         String value = mSharedPrefs.getString(REFRESH_CONTROL, null);
 
         if (value == null || value.isEmpty()) {
-            value = REFRESH_STANDARD + ":" + REFRESH_EXTREME;
+            value = REFRESH_MEDIUM + ":" + REFRESH_MAXIMUM;
             writeValue(value);
         }
         return value;
@@ -86,10 +86,10 @@ public final class RefreshUtils {
         String finalString;
 
         switch (mode) {
-            case STATE_STANDARD:
+            case STATE_MEDIUM:
                 modes[0] = modes[0] + packageName + ",";
                 break;
-            case STATE_EXTREME:
+            case STATE_MAXIMUM:
                 modes[1] = modes[1] + packageName + ",";
                 break;
         }
@@ -104,9 +104,9 @@ public final class RefreshUtils {
         String[] modes = value.split(":");
         int state = STATE_DEFAULT;
         if (modes[0].contains(packageName + ",")) {
-            state = STATE_STANDARD;
+            state = STATE_MEDIUM;
         } else if (modes[1].contains(packageName + ",")) {
-            state = STATE_EXTREME;
+            state = STATE_MAXIMUM;
         }
         return state;
     }
@@ -122,13 +122,13 @@ public final class RefreshUtils {
             modes = value.split(":");
 
             if (modes[0].contains(packageName + ",")) {
-                maxrate = REFRESH_STATE_STANDARD;
+                maxrate = REFRESH_STATE_MEDIUM;
                 if ( minrate > maxrate){
                 minrate = maxrate;
                 }
 		isAppInList = true;
            } else if (modes[1].contains(packageName + ",")) {
-                maxrate = REFRESH_STATE_EXTREME;
+                maxrate = REFRESH_STATE_MAXIMUM;
                 if ( minrate > maxrate){
                 minrate = maxrate;
                 }
