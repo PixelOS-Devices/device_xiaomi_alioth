f3289f92fd81998b6a38a4d31485959278a72bf5
johnmart19 <johnivan19999@gmail.com>
2023-05-27 04:45:38 +0300

    Alioth: Inherit Updated Miui Camera Repository
    - Includes MiSys and cleaned deps
    - https://gitlab.com/johnmart19/vendor_xiaomi_camera
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Miui Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
