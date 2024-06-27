a1b6f5a8287e39c105c688389ede163b628ee850
NotZeetaa <rodrigo2005contente@gmail.com>
2024-06-27 08:40:15 +0000

    alioth: Aospify
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- device.mk
+++ device.mk
@@ -31,8 +31,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
