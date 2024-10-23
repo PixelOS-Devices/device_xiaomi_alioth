yoriscape/android_device_xiaomi_alioth

Alioth: Libinit: Add support of .mod_device hides
* Required hide for Miui Apps

libinit/init_xiaomi_alioth.cpp
@@ -13,6 +13,7 @@ static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
+    .mod_device = "alioth_in_global",
     .brand = "Xiaomi",
     .device = "aliothin",
     .marketname = "Mi 11X",
@@ -26,6 +27,7 @@ static const variant_info_t alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
+    .mod_device = "alioth_in_global",
     .brand = "POCO",
     .device = "alioth",
     .marketname = "POCO F3",
@@ -39,6 +41,7 @@ static const variant_info_t alioth_info = {
     .hwc_value = "",
     .sku_value = "",
 
+    .mod_device = "alioth_in_global",
     .brand = "Redmi",
     .device = "alioth",
     .marketname = "K40",

PixelOS-Devices/device_xiaomi_alioth

Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

libinit/init_xiaomi_alioth.cpp
@@ -13,6 +13,7 @@ static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
+    .mod_device = "alioth_global",
     .brand = "Mi",
     .device = "aliothin",
     .marketname = "Mi 11X",
@@ -26,6 +27,7 @@ static const variant_info_t alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
+    .mod_device = "alioth_global",
     .brand = "POCO",
     .device = "alioth",
     .marketname = "POCO F3",
@@ -39,6 +41,7 @@ static const variant_info_t alioth_info = {
     .hwc_value = "",
     .sku_value = "",
 
+    .mod_device = "alioth_global",
     .brand = "Redmi",
     .device = "alioth",
     .marketname = "Redmi K40",