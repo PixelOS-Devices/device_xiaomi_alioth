5ae389d213cc8d1439565facfe2cf9a46ad1d182
Tkiliay <sackclouths@gmail.com>
2025-06-14 23:10:57 +0800

    Revert "alioth: Implement DSPVolumeSynchronizer"
    Build from source
    
    This reverts commit 88736954e5831dd6a5cc0f840e951fbf22ab8003.
    
    Change-Id: I5aa9ac71524010c78f703ca16a836132beb578eb

--- device.mk
+++ device.mk
@@ -32,10 +32,6 @@ PRODUCT_PACKAGES += \
 # Miui Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 
-# DSP Volume Synchronizer
-PRODUCT_PACKAGES += \
-    DSPVolumeSynchronizer
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
